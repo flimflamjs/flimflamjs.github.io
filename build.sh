281faebc13492622fffa09b8718d2dee634c1a82
Jay R Bolton <jayrbolton@gmail.com>
2017-01-24 18:13:38 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir -p dist
+mkdir -p dist/markdown-articles
 cp -r index.html dist/
 cp -r styles/ dist/
 cp -r images/ dist/
-cp *.md dist/
-browserify -t babelify index.es6 | uglifyjs -cm > dist/index.js
+cp markdown-articles/*.md dist/markdown-articles/
+browserify -t es2040 index.js | uglifyjs -cm > dist/index.js
